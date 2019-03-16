7b4fc244d71827668f7585294bc23a8c272ce011
nedsonson <nedackland@googlemail.com>
2019-03-16 12:03:34 +0000

    BT23 projectile flight

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,9 +2,11 @@
 
 #pragma once
 
+#include "Public/UObject/Class.h"
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h"//put new includes above
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -13,6 +15,12 @@ class BATTLETANK_API ATank : public APawn
 
 public:
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
