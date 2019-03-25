dbdc043c08c04955dfdee478736345eb521a46ba
nedsonson <nedackland@googlemail.com>
2019-03-25 11:46:00 +0000

    BT86 Mortars

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -104,11 +104,11 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 	
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if(DeltaRotator.Yaw < 180)
+	if(FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else 
+	else //avoid going long way
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
